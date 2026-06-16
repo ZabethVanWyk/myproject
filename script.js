313a44fda283af20dd8ba6a930aeeca9ba81a3a8
Zabeth van Wyk <zabethvanwyk@gmail.com>
2026-06-16 11:12:37 +0100

    Adding images

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const cards = [
         topText: "More than",
         statistic: "12,500,000",
         bottomText: "delighted guests",
-        colour: "green"
+        colour: "green",
+        image: "images/hand.svg"
     },
     {
         topText: "Find us in",
         statistic: "50",
         bottomText: "countries",
-        colour: "pink"
+        colour: "pink",
+        image: "images/earth.svg"
     },
     {
         topText: "Over",
         statistic: "800,000,000",
         bottomText: "darts thrown",
-        colour: "blue"
+        colour: "blue",
+        image: "images/dart.svg"
     }
 ];
 
@@ -28,9 +31,12 @@ cards.forEach(card => {
     article.className = `card ${card.colour}`;
 
     article.innerHTML = `
-  <p class="top-text">${card.topText}</p>
-  <h2>${card.statistic}</h2>
-  <p class="bottom-text">${card.bottomText}</p>
+    <div class="card-content">
+        <p class="top-text">${card.topText}</p>
+        <h2>${card.statistic}</h2>
+        <p class="bottom-text">${card.bottomText}</p>
+    </div>
+    <img src="${card.image}" alt="" class="card-image">
 `;
 
     container.appendChild(article);
