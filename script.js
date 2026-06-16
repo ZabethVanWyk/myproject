4bb4062eed2c3fe66bb7685cfaf5effe8c4a408e
Zabeth van Wyk <zabethvanwyk@gmail.com>
2026-06-16 11:59:27 +0100

    Refining hover

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ const cards = [
         statistic: "12,000,000",
         bottomText: "delighted guests",
         colour: "green",
-        image: "images/hand.svg"
+        image: "images/hand.svg",
+        alt: "Raised hand illustration"
     },
     {
         topText: "Find us in",
         statistic: "50",
         bottomText: "countries",
         colour: "pink",
-        image: "images/earth.svg"
+        image: "images/earth.svg",
+        alt: "line drawing of the earth illustration"
     },
     {
         topText: "Over",
         statistic: "800,000,000",
         bottomText: "darts thrown",
         colour: "blue",
-        image: "images/dart.svg"
+        image: "images/dart.svg",
+        alt: "dart illustration"
     }
 ];
 
@@ -36,7 +39,7 @@ cards.forEach(card => {
         <h2>${card.statistic}</h2>
         <p class="bottom-text">${card.bottomText}</p>
     </div>
-    <img src="${card.image}" alt="" class="card-image">
+    <img src="${card.image}" alt="${card.alt}" class="card-image">
 `;
 
     container.appendChild(article);
